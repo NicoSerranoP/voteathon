a1d8f0bfdf45262aaaf32d643a6167948e525df5
NicoSerranoP <nnicosp@hotmail.com>
2023-06-12 15:59:27 -0400

    contracts(feat): NFT prizes

---
+++ packages/contracts/contracts/VoteathonNFT.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ERC721URIStorage} "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Counters} "@openzeppelin/contracts/utils/Counters.sol";
+
+contract VoteathonNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("VoteathonNFT", "VoteNFT") {}
+
+    function awardItem(address winner, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        uint256 newItemId = _tokenIds.current();
+        _mint(winner, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        _tokenIds.increment();
+        return newItemId;
+    }
+}
