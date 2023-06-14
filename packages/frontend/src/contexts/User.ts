e4e3c00303a70134d133ab34391ab0ecac2c4cbe
jacque006 <jacque006@users.noreply.github.com>
2023-06-14 17:17:36 -0400

    Remove empty string as default secret, allow storage clearing

--- packages/frontend/src/contexts/User.ts
+++ packages/frontend/src/contexts/User.ts
@@ -23,8 +23,8 @@ class User {
     }
 
     async load() {
-        const id: string = localStorage.getItem('id') ?? ''
-        const identity = new Identity(id)
+        const id: string | null = localStorage.getItem('id')
+        const identity = new Identity(id ?? undefined)
         if (!id) {
             localStorage.setItem('id', identity.toString())
         }
