26d5e6282ca3a35321147e8660c07a54783fb329
NicoSerranoP <nnicosp@hotmail.com>
2023-06-14 15:58:50 -0400

    frontend(feat): avoid header in all pages

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Header from './pages/Header'
 import Start from './pages/Start'
 import Dashboard from './pages/Dashboard'
 import './index.css'
@@ -10,10 +8,8 @@ export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Header />}>
-                    <Route index element={<Start />} />
-                    <Route path="dashboard" element={<Dashboard />} />
-                </Route>
+                <Route path="/" element={<Start />} />
+                <Route path="dashboard" element={<Dashboard />} />
             </Routes>
         </BrowserRouter>
     )
