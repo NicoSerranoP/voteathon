97f0c3447b5eb6b4da37f4637f757dda021b006a
NicoSerranoP <nnicosp@hotmail.com>
2023-06-16 16:39:04 -0400

    frontend(feat): claim btn added

--- packages/frontend/src/components/Header.tsx
+++ packages/frontend/src/components/Header.tsx
@@ -3,27 +3,50 @@ import VoteathonIcon from '../assets/logo.svg'
 import GithubIcon from '../assets/github.svg'
 
 const Header = () => {
+    const onClickClaim = () => {
+        console.log('claiming reward')
+    }
+
     return (
         <Container>
-            <LogoContainer>
-                <img src={VoteathonIcon} width={'40px'}></img>
-                <h1>voteathon</h1>
-            </LogoContainer>
-            <img src={GithubIcon} width={'30px'}></img>
+            <RightContainer>
+                <img src={VoteathonIcon} width={'40px'} />
+                <h4>voteathon</h4>
+            </RightContainer>
+            <LeftContainer>
+                <ClaimButton onClick={onClickClaim}>Claim reward</ClaimButton>
+                <img src={GithubIcon} width={'30px'} />
+            </LeftContainer>
         </Container>
     )
 }
 
-const LogoContainer = styled.div`
+const RightContainer = styled.div`
     gap: 10px;
     display: flex;
     align-items: center;
+`
+
+const LeftContainer = styled.div`
+    gap: 30px;
+    display: flex;
+    align-items: center;
+`
+
+const ClaimButton = styled.button`
+    background: #ffe94d;
+    border-radius: 10px;
+    border: none;
+    padding-inline: 20px;
+    padding-block: 10px;
+    font-family: inherit;
 `
 
 const Container = styled.div`
     display: flex;
     padding-inline: 20px;
     min-height: 80px;
+    align-items: center;
     justify-content: space-between;
 `
 
