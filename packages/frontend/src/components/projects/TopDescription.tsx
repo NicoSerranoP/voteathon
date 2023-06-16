7c1552be0b00fde8cc89bc345faec6a6405ecc4c
NicoSerranoP <nnicosp@hotmail.com>
2023-06-16 16:31:52 -0400

    frontend(feat): use start & end dates as constants

--- packages/frontend/src/components/projects/TopDescription.tsx
+++ packages/frontend/src/components/projects/TopDescription.tsx
@@ -1,5 +1,15 @@
 import { useState } from 'react'
 import { styled } from 'styled-components'
+import { END_DATE, START_DATE } from '../../constants'
+
+const startHour = new Date(START_DATE).toLocaleTimeString('en-US')
+const startDay = new Date(START_DATE).getDate()
+const startMonth = new Date(START_DATE).toLocaleString('default', {
+    month: 'long',
+})
+const endHour = new Date(END_DATE).toLocaleTimeString('en-US')
+const endDay = new Date(END_DATE).getDate()
+const endMonth = new Date(END_DATE).toLocaleString('default', { month: 'long' })
 
 const TopDescription = () => {
     // TODO: get the epoch start & epoch end
@@ -11,25 +21,42 @@ const TopDescription = () => {
     return (
         <Container>
             <Title>{isVotingTime ? 'Projects' : 'Voting result'}</Title>
-            <Description>
-                {isVotingTime
-                    ? `Total of ${numberOfProjects} projects. Only 1 vote per person and you can’t vote on your project.`
-                    : `${numberOfVoters} people voted.`}
-            </Description>
-            <Highlight>
-                {isVotingTime
-                    ? `Voting ends: hh:mm:ss`
-                    : `Voting starts at: Date. Close at: Date`}
-            </Highlight>
-            <BlockDescription>
-                {isVotingTime
-                    ? 'Voting started. LFG'
-                    : isResultTime &&
-                      'You already voted. Let’s wait for result.'}
-            </BlockDescription>
+            <BlockText>
+                <Description>
+                    {isVotingTime
+                        ? `Total of ${numberOfProjects} projects. Only 1 vote per person and you can’t vote on your project.`
+                        : `${numberOfVoters} people voted.`}
+                </Description>
+                <Highlight>
+                    {isVotingTime
+                        ? `Voting ends: hh:mm:ss`
+                        : `Voting starts at: ${startMonth} ${startDay}, ${startHour}. Close at: ${endMonth} ${endDay}, ${endHour}.`}
+                </Highlight>
+                <BlockDescription>
+                    {isVotingTime
+                        ? 'Voting started. LFG'
+                        : isResultTime &&
+                          'You already voted. Let’s wait for results.'}
+                </BlockDescription>
+            </BlockText>
         </Container>
     )
 }
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+const BlockText = styled.div`
+    max-width: 70%;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    gap: 6px;
+`
 
 const Title = styled.h1`
     margin: 0px;
@@ -37,14 +64,13 @@ const Title = styled.h1`
 
 const Description = styled.p`
     margin-block: 12px;
-    max-width: 70%;
     line-height: 16px;
-    text-align: center;
 `
 
 const Highlight = styled.p`
     margin-block: 5px;
     font-weight: 800;
+    line-height: 16px;
 `
 
 const BlockDescription = styled.h5`
@@ -54,13 +80,6 @@ const BlockDescription = styled.h5`
     background: #fcfcfc;
     padding-inline: 10px;
     padding-block: 10px;
-`
-
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
 `
 
 export default TopDescription
