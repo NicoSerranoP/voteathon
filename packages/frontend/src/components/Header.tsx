bcc0606f68ee57200f28c9037ba4adf20e656c51
NicoSerranoP <nnicosp@hotmail.com>
2023-06-16 15:27:47 -0400

    frontend(feat): header with 2 icons + black body

--- packages/frontend/src/components/Header.tsx
+++ packages/frontend/src/components/Header.tsx
@@ -1,20 +1,30 @@
 import { styled } from 'styled-components'
 import VoteathonIcon from '../assets/logo.svg'
+import GithubIcon from '../assets/github.svg'
 
 const Header = () => {
     return (
         <Container>
-            <img src={VoteathonIcon}></img>
-            <h1>voteathon</h1>
+            <LogoContainer>
+                <img src={VoteathonIcon} width={'40px'}></img>
+                <h1>voteathon</h1>
+            </LogoContainer>
+            <img src={GithubIcon} width={'30px'}></img>
         </Container>
     )
 }
 
-const Container = styled.div`
-    width: 100%;
+const LogoContainer = styled.div`
+    gap: 10px;
     display: flex;
-    background: red;
+    align-items: center;
+`
+
+const Container = styled.div`
+    display: flex;
+    padding-inline: 20px;
     min-height: 80px;
+    justify-content: space-between;
 `
 
 export default Header
