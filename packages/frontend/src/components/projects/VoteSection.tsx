61aa48773fc1fc2c8233483c0e09523f7ad6098b
NicoSerranoP <nnicosp@hotmail.com>
2023-06-18 12:36:03 -0400

    frontend(feat): block people from voting if alreadyvoted

--- packages/frontend/src/components/projects/VoteSection.tsx
+++ packages/frontend/src/components/projects/VoteSection.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { styled } from 'styled-components'
 import VoteathonIconBlack from '../../assets/logo-black.svg'
+import PathCheckIcon from '../../assets/path-check.svg'
 import VotingModal from './VotingModal'
 
 type Props = {
@@ -9,23 +10,47 @@ type Props = {
 }
 
 const VoteSection = ({ projectId, projectName }: Props) => {
+    const [alreadyVoted, setAlreadyVoted] = useState(true)
     const [open, setOpen] = useState(false)
+
+    useEffect(() => {
+        const alreadyVoted = localStorage.getItem('alreadyVoted')
+        if (!alreadyVoted) {
+            setAlreadyVoted(false)
+        } else {
+            setAlreadyVoted(true)
+        }
+    }, [])
 
     const handleVoteClick = () => {
         setOpen(true)
     }
 
     return (
-        <Container>
-            <img src={VoteathonIconBlack} width={'90px'} />
-            <Button onClick={handleVoteClick}>Vote for me</Button>
-            <VotingModal
-                open={open}
-                projectId={projectId}
-                projectName={projectName}
-                onDeselect={() => setOpen(false)}
-            />
-        </Container>
+        <>
+            {alreadyVoted ? (
+                <Container
+                    style={{
+                        background: 'transparent',
+                        textAlign: 'center',
+                    }}
+                >
+                    <img src={PathCheckIcon} width={'20px'} />
+                    <p style={{ lineHeight: '15px' }}>You already voted.</p>
+                </Container>
+            ) : (
+                <Container>
+                    <img src={VoteathonIconBlack} width={'90px'} />
+                    <Button onClick={handleVoteClick}>Vote for me</Button>
+                    <VotingModal
+                        open={open}
+                        projectId={projectId}
+                        projectName={projectName}
+                        onDeselect={() => setOpen(false)}
+                    />
+                </Container>
+            )}
+        </>
     )
 }
 
